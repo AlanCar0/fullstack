2f0c59dae8398b69088f69243aa3d4d042bd7efc
Alansito-kun <al.carov@duocuc.cl>
2025-05-14 10:51:21 -0400

    Main

---
+++ Parcial/Fullstack/src/main/java/com/Fullstack/Fullstack/model/Cliente.java
@@ -0,0 +1,32 @@
+package com.Fullstack.Fullstack.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cliente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @Column (length = 10)
+    private String run;
+
+    @Column (nullable = false, length = 50)
+    private String nombre;
+    @Column (nullable = false, length = 50)
+    private String apellido;
+    @Column (nullable = false,length = 50)
+    private String email;
+    @Column (nullable = false,length = 50)
+    private String telefono;
+    @Column (nullable = false, length = 50)
+    private String direccion;
+    @Column (nullable = false, name = "FECHA_NACIMIENTO",length = 10)
+    private String fec_naciemiento;
+
+}
+
+
